0e9d273d3e9294208e57e4b64748a81bf190570e
Toni-Matthias Graeser <Toni-Matthias.Graeser@web4train.de>
2021-11-18 15:43:27 +0100

    Projektdateien hinzufügen.

---
+++ Bierpong_2/Team.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bierpong_2
+{
+    public enum TeamProperties
+    {
+        Kapitän,
+        Teamname
+    };
+    public class Team
+    {
+        public string Kapitän;
+        public string Teamname;
+        public int GruppenphasePunkte;
+
+        public static List<Team> AlleTeams = new();
+
+        public Team(string tmn, string cptname)
+        {
+            try
+            {
+                if (CaptainOrTeamNameIsNotEmptyOrNull(tmn))
+                {
+                    Teamname = tmn;
+                }
+                else
+                {
+                    ///
+                }
+
+                if (CaptainOrTeamNameIsNotEmptyOrNull(cptname))
+                {
+                    Kapitän = cptname;
+                }
+                else
+                {
+                    ///
+                }
+                AddTeamToTheList(this);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        public bool CaptainOrTeamNameIsNotEmptyOrNull(string name)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(name.Trim()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void ChangeTeamOrCaptainName(string name, TeamProperties property)
+        {
+            if (property == TeamProperties.Kapitän)
+            {
+                if (this.CaptainOrTeamNameIsNotEmptyOrNull(name))
+                {
+                    Kapitän = name;
+                }
+            }
+
+            else
+            {
+                if (this.CaptainOrTeamNameIsNotEmptyOrNull(name))
+                {
+                    Teamname = name;
+                }
+            }
+        }
+
+        public void AddTeamToTheList(Team tm)
+        {
+            AlleTeams.Add(tm);
+        }
+
+        public void DeleteTeamFromTheList()
+        {
+            AlleTeams.Remove(this);
+        }
+
+        public void AddGroupphasePoints()
+        {
+            GruppenphasePunkte += 3;
+        }
+
+        public void SubstractGroupphasePoints()
+        {
+            GruppenphasePunkte -= 3;
+        }
+    }
+}
---
+++ Bierpong_2_Tests/Team_Tests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Bierpong_2;
+using NUnit.Framework;
+
+namespace Bierpong_2_Tests
+{
+    public class Team_Tests
+    {
+        private Team MakeTeam(string tmn = "Alkoholis", string cptn = "Egon")
+        {
+            return new Team(tmn, cptn);
+        }
+
+        [TestCase("Egon", true)]
+        [TestCase("      ", false)]
+        [TestCase(".", true)]
+        [TestCase("", false)]
+        public void CaptainOrTeamNameNotEmptyOrNull_Various_CheckThem(string name, bool expected)
+        {
+             Team myTeam = MakeTeam(name);
+
+            bool result = myTeam.CaptainOrTeamNameIsNotEmptyOrNull(name);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestCase("       ", false, TeamProperties.Kapitän)]
+        [TestCase("Manfred", true, TeamProperties.Kapitän)]
+        [TestCase(null, false, TeamProperties.Kapitän)]
+        public void ChangeTeamOrCaptainName_CheckVariousCaptainNames_CheckThem(string changedname, bool expected, TeamProperties prop = TeamProperties.Kapitän)
+        {
+            Team myTeam = MakeTeam();
+
+            var tmp = myTeam.Kapitän;
+            myTeam.ChangeTeamOrCaptainName(changedname, prop);
+            bool result = (tmp != myTeam.Kapitän);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(" ", false, TeamProperties.Teamname)]
+        [TestCase("PrimaDonnas", true, TeamProperties.Teamname)]
+        [TestCase(null, false, TeamProperties.Teamname)]
+        public void ChangeTeamOrCaptainName_CheckVariousTeamNames_CheckThem(string changedname, bool expected, TeamProperties prop = TeamProperties.Teamname)
+        {
+            Team myTeam = MakeTeam();
+
+            var tmp = myTeam.Teamname;
+            myTeam.ChangeTeamOrCaptainName(changedname, prop);
+            bool result = (tmp != myTeam.Teamname);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void AddGroupphasePoints_CheckIfTheMethodAddsThePoints_ReturnTrue()
+        {
+            Team myt = MakeTeam();
+
+            myt.AddGroupphasePoints();
+            bool result = (myt.GruppenphasePunkte == 3);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void SubstractGroupphasePoints_CheckIfTheMethodSubstractThePoints_ReturnTrue()
+        {
+            Team myt = MakeTeam();
+
+            myt.SubstractGroupphasePoints();
+            bool result = myt.GruppenphasePunkte == -3;
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void AddTeamToTheList_CheckIfTheMethodAddsTheTeamToTheList_ReturnTrue()
+        {
+            Team myt = MakeTeam();
+
+            bool result = Team.AlleTeams.Count > 1;
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void DeleteTeamFromTheList_CheckIfTheMethodDeletesTheTeamFromTheList_ReturnTrue()
+        {
+            Team myt = MakeTeam();
+
+            myt.DeleteTeamFromTheList();
+            bool result = (Team.AlleTeams.Count == 0);
+
+            Assert.IsTrue(result);
+
+        }
+    }
+}
